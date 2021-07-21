dbee5eeb2f17141e2c7c5d11fdf25500ed49656a
neko113 <alstn113@gmail.com>
2021-07-21 18:16:26 +0900

    sdfafsdf

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
+    username: "root",
+    password: process.env.DEV_PASSWORD,
+    database: "deploy",
+    host: "127.0.0.1",
     dialect: "mysql",
     logging: false,
   },
--- routes/page.js
+++ routes/page.js
@@ -2,24 +2,21 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const user = User.findAll({});
-  res.render("test1", { User: user });
+router.get("/", async (req, res) => {
+  res.render("test1");
 });
 
-router.get("/test", (req, res) => {
-  res.render("test2");
+router.get("/test", async (req, res) => {
+  const user = await User.findAll({});
+  res.render("test2", { User: user });
 });
 
-router.post("/create", (req, res) => {
-  User.create({
+router.post("/create", async (req, res) => {
+  await User.create({
     nick: req.body.nick,
     age: req.body.age,
   });
-});
-
-router.get("/give", (req, res) => {
-  return res.json({ nickname: "김민수" });
+  res.redirect("/");
 });
 
 module.exports = router;
