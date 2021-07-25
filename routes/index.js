bdfe18eba2db533783f932627b200861ea301ea2
neko113 <alstn113@gmail.com>
2021-07-25 11:18:30 +0900

    t1

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const user = require("./user");
 const post = require("./post");
 
-router.use("/user", user);
 router.use("/post", post);
 
 module.exports = router;
