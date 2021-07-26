beaa632eeddef2cd50147b5c4f76a5264e6685a5
neko113 <alstn113@gmail.com>
2021-07-26 11:28:30 +0900

    infinite

--- models/index.js
+++ models/index.js
@@ -1,17 +1,12 @@
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
-const User = require("./user");
 const Post = require("./post");
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-
-db.User = User;
-User.init(sequelize);
-User.associate(db);
 
 db.Post = Post;
 Post.init(sequelize);
--- routes/post/controller.js
+++ routes/post/controller.js
@@ -13,6 +13,8 @@ exports.getPostDetail = async (req, res, next) => {
   const { id } = req.params;
   const postDetail = await Post.findOne({
     where: { id },
+    raw: true,
+    nest: true,
   });
   res.json({ postDetail });
 };
@@ -26,6 +28,26 @@ exports.deletePost = async (req, res, next) => {
 };
 
 exports.getPostList = async (req, res, next) => {
-  const postList = await Post.findAll({});
-  res.json({ postList });
+  let options = {};
+  const take = 5;
+  if (req.query.page) {
+    const page = parseInt(req.query.page) || 1;
+    options = {
+      ...options,
+      limit: 5, // 몇 개 가져올지
+      offset: (page - 1) * take, // 몇 개 뛰어넘을지
+      //프론트에서 사용할만큼만 주고 붙여서 사용함.
+    };
+  }
+  const total = await Post.count({
+    ...options,
+  });
+  const lastpage = Math.ceil(total / take);
+  const postList = await Post.findAll({
+    raw: true,
+    nest: true,
+    order: [["createdAt", "DESC"]],
+    ...options,
+  });
+  res.json({ postList, lastpage });
 };
